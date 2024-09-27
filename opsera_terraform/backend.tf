ca9afeb5e26d828baaaf9a0fff8d9f788ceeef09
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2024-09-27 13:17:14 +0530

    Update backend.tf

--- opsera_terraform/backend.tf
+++ opsera_terraform/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+ backend "s3" {
+   bucket         = var.bucket #"my-terraform-state-bucket"
+   key            = var.terraformstate_key #"vpc/terraform.tfstate"
+   region         = var.region #"us-east-1"
+   dynamodb_table = var.dynamodb_table #"terraform-lock-table"
+   encrypt        = true
+ }
+}
