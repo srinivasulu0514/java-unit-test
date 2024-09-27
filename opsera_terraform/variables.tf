a50747b8108c013b3d2130fe0a0e0e632c3f3d97
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2024-09-27 13:23:26 +0530

    Update variables.tf

--- opsera_terraform/variables.tf
+++ opsera_terraform/variables.tf
@@ -1,13 +1,19 @@
-variable "instance_type" {
- description = "EC2 instance type"
- default     = "t2.micro"
+variable "bucket" {
+ description = "Enter your s3 bucket name"
+ type = string
 }
 
-variable "instance_type" {
- description = "EC2 instance type"
- default     = "t2.micro"
+variable "terraformstate_key" {
+ description = "Enter your terraform statefile name"
+ type = string
 }
-variable "instance_type" {
- description = "EC2 instance type"
- default     = "t2.micro"
+
+variable "region" {
+ description = "Enter AWS region"
+ type = string
+}
+
+variable "dynamodb_table" {
+ description = "Enter dynamodb table name for statefile locking"
+ type = string
 }
